a863b820e4438f27e3f1bf569bd0744c039c44f4
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2020-05-06 15:01:58 +0900

    #3 13-5 switch 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -28,6 +28,7 @@ namespace cscondition
 
 
             // 오전과 오후 구분
+            /*
             Console.WriteLine(DateTime.Now.Year);
             Console.WriteLine(DateTime.Now.Month);
             Console.WriteLine(DateTime.Now.Day);
@@ -50,6 +51,32 @@ namespace cscondition
             else { 
                 Console.WriteLine("저녁 먹을 시간입니다.");
                 
+            }
+            */
+            var input = int.Parse(Console.ReadLine());
+            switch (input % 2)
+            {
+                case 0: Console.WriteLine("짝수 입니다"); break;
+                case 1: Console.WriteLine("홀수 입니다"); break;
+            }
+
+            Console.Write("이번 달은 몇 월인가요 : ");
+            int input2= int.Parse(Console.ReadLine());
+            switch (input2)
+            {
+                case 12: 
+                case 2: 
+                case 1:  Console.WriteLine("겨울입니다."); break;
+                case 3:
+                case 4: 
+                case 5: Console.WriteLine("봄입니다."); break;
+                case 6: 
+                case 7: 
+                case 8: Console.WriteLine("여름입니다."); break;
+                case 9: 
+                case 10: 
+                case 11: Console.WriteLine("가을입니다."); break;
+                default: Console.WriteLine("인식 불가합니다."); break;
             }
         }
     }
